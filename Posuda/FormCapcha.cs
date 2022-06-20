5d48ed47d8cdf85f37e341265527d79650a97c4e
Михаил Кораблев <cstud110917@vyatsu.ru>
2022-06-20 19:42:04 +0300

    go

--- Posuda/FormCapcha.cs
+++ Posuda/FormCapcha.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Posuda
 {
@@ -22,85 +23,59 @@ namespace Posuda
         {
             Random rnd = new Random();
 
-            //Создадим изображение
-            Bitmap result = new Bitmap(Width, Height);
+                //Создадим изображение
+                Bitmap result = new Bitmap(Width, Height);
 
-            //Вычислим позицию текста
-            int Xpos = 10;
-            int Ypos = 10;
+                //Вычислим позицию текста
+                int Xpos = 10;
+                int Ypos = 10;
 
-            //Добавим различные цвета ддя текста
-            Brush[] colors = {
-             Brushes.Black,
-             Brushes.Red,
-             Brushes.RoyalBlue,
-             Brushes.Green,
-             Brushes.Yellow,
-             Brushes.White,
-             Brushes.Tomato,
-             Brushes.Sienna,
-             Brushes.Pink };
+                //Добавим различные цвета ддя текста
+                Brush[] colors = {
+Brushes.Black,
+};
 
-            //Добавим различные цвета линий
-            Pen[] colorpens = {
-             Pens.Black,
-             Pens.Red,
-             Pens.RoyalBlue,
-             Pens.Green,
-             Pens.Yellow,
-             Pens.White,
-             Pens.Tomato,
-             Pens.Sienna,
-             Pens.Pink };
+                //Добавим различные цвета линий
+                Pen[] colorpens = {
+Pens.Black,
+};
 
-            //Делаем случайный стиль текста
-            FontStyle[] fontstyle = {
-             FontStyle.Bold,
-             FontStyle.Italic,
-             FontStyle.Regular,
-             FontStyle.Strikeout,
-             FontStyle.Underline};
+                //Делаем случайный стиль текста
+                FontStyle[] fontstyle = {
+FontStyle.Italic,
+};
 
-            //Добавим различные углы поворота текста
-            Int16[] rotate = { 1, -1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6 };
+                //Добавим различные углы поворота текста
+                Int16[] rotate = { 1, -1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6 };
 
-            //Укажем где рисовать
-            Graphics g = Graphics.FromImage((Image)result);
+                //Укажем где рисовать
+                Graphics g = Graphics.FromImage((Image)result);
 
-            //Пусть фон картинки будет серым
-            g.Clear(Color.Gray);
 
-            //Делаем случайный угол поворота текста
-            g.RotateTransform(rnd.Next(rotate.Length));
 
-            //Генерируем текст
-            text = String.Empty;
-            string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
-            for (int i = 0; i < 5; ++i)
-                text += ALF[rnd.Next(ALF.Length)];
+                //Делаем случайный угол поворота текста
+                g.RotateTransform(rnd.Next(rotate.Length));
 
-            //Нарисуем сгенирируемый текст
-            g.DrawString(text,
-                         new Font("Arial", 25, fontstyle[rnd.Next(fontstyle.Length)]),
-                         colors[rnd.Next(colors.Length)],
-                         new PointF(Xpos, Ypos));
+                //Генерируем текст
+                text = String.Empty;
+                string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+                for (int i = 0; i < 5; ++i)
+                    text += ALF[rnd.Next(ALF.Length)];
 
-            //Добавим немного помех
-            //Линии из углов
-            g.DrawLine(colorpens[rnd.Next(colorpens.Length)],
-                       new Point(0, 0),
-                       new Point(Width - 1, Height - 1));
-            g.DrawLine(colorpens[rnd.Next(colorpens.Length)],
-                       new Point(0, Height - 1),
-                       new Point(Width - 1, 0));
+                //Нарисуем сгенирируемый текст
+                g.DrawString(text,
+                new Font("Arial", 15, fontstyle[rnd.Next(fontstyle.Length)]),
+                colors[rnd.Next(colors.Length)],
+                new PointF(Xpos, Ypos));
 
-            //Белые точки
-            for (int i = 0; i < Width; ++i)
-                for (int j = 0; j < Height; ++j)
-                    if (rnd.Next() % 20 == 0)
-                        result.SetPixel(i, j, Color.White);
+                //Добавим немного помех
+                //Линии из углов
+                g.DrawLine(colorpens[rnd.Next(colorpens.Length)],
+                new Point(0, 0),
+                new Point(Width - 1, Height - 1));
 
-            return result;
+                return result;
+      
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -116,6 +91,7 @@ namespace Posuda
                 FormAuthorization auto = new FormAuthorization();
                 auto.Show();
                 this.Hide();
+                Thread.Sleep(5000);
             }
             else
             {
